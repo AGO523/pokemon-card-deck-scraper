b0f5663847859ae4da9ddb0425de37f99ddf8555
AGO523 <unpouletfrais0523@gmail.com>
2024-05-11 10:33:11 +0900

    add: Dockerfile & github actions

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
-FROM node:14-slim
+FROM node:20@sha256:cb7cd40ba6483f37f791e1aace576df449fc5f75332c19ff59e2c6064797160e
+ENV LANG ja_JP.UTF-8
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg \
+    && sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] https://dl-ssl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-khmeros fonts-kacst fonts-freefont-ttf libxss1 dbus dbus-x11 --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/* \
+    && groupadd -r pptruser && useradd -rm -g pptruser -G audio,video pptruser
 
-RUN apt-get update && apt-get install -y wget ca-certificates fonts-liberation libappindicator3-1 xdg-utils \
-    libasound2 libatk-bridge2.0-0 libnspr4 libnss3 libxss1 lsb-release xdg-utils libatk1.0-0 libgtk-3-0 libgbm-dev
 
-WORKDIR /app
-
-COPY package*.json ./
-
-RUN npm install
-
+WORKDIR /home/pptruser
 COPY . .
-
-CMD ["node", "app.js"]
-
+RUN npm install
 EXPOSE 3000
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const puppeteer = require("puppeteer");
+const { Storage } = require("@google-cloud/storage");
+
+// 環境変数からバケット名を取得
+const bucketName = process.env.GCS_BUCKET;
+const storage = new Storage();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post("/fetchDeck", async (req, res) => {
+  const { deckCode } = req.body;
+  if (!deckCode) {
+    return res.status(400).send({ message: "Deck code is required" });
+  }
+
+  try {
+    const screenshotUrl = await accessPokemonCardSite(deckCode);
+    res.send({ message: "Deck fetched successfully", data: screenshotUrl });
+  } catch (error) {
+    console.error("Failed to fetch deck:", error);
+    res.status(500).send({
+      message: "Failed to fetch deck due to an error: " + error.message,
+    });
+  }
+});
+
+async function accessPokemonCardSite(deckCode) {
+  let browser;
+  try {
+    console.log("Launching browser...");
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: "/usr/bin/google-chrome-stable",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process",
+        "--disable-gpu",
+      ],
+    });
+
+    const page = await browser.newPage();
+    console.log("Opening new page...");
+    await page.goto("https://www.pokemon-card.com/deck/", {
+      waitUntil: "networkidle2",
+    });
+
+    page.on("dialog", async (dialog) => {
+      await dialog.accept();
+    });
+
+    console.log("Typing deck ID...");
+    await page.type("#deckID", deckCode);
+    console.log("Clicking search button...");
+    await page.click("#searchDeckView");
+    await page.waitForNavigation({
+      waitUntil: "networkidle2",
+    });
+
+    console.log("Clicking regulation check button...");
+    await page.click("#fr_regulationChekcBtn");
+    await sleep(2000);
+
+    console.log("Registering deck data...");
+    await page.click("#fr_registDeckData");
+    await sleep(2000);
+
+    console.log("Waiting for image button to become visible...");
+    await page.waitForSelector("#deckImgeBtn", { visible: true });
+
+    console.log("Clicking image button...");
+    await page.click("#deckImgeBtn");
+
+    const newPage = await new Promise((resolve) =>
+      browser.once("targetcreated", (target) => resolve(target.page()))
+    );
+    console.log("New page opened for deck image...");
+    await newPage
+      .waitForSelector(".PopupMain", { visible: true })
+      .catch((e) => console.error("PopupMain selector not found:", e));
+
+    console.log("Taking screenshot...");
+    const buffer = await newPage.screenshot();
+    const screenshotPath = `screenshots/${deckCode}_final.png`;
+    console.log("Screenshot taken successfully:", screenshotPath);
+
+    //   console.log("Uploading screenshot to Google Cloud Storage...");
+    //   await uploadBufferToGCS(buffer, screenshotPath);
+
+    //   await browser.close();
+    //   console.log("Browser closed successfully.");
+    //   return `https://storage.googleapis.com/${bucketName}/${screenshotPath}`;
+  } catch (error) {
+    if (browser) {
+      console.error("Closing browser due to an error...");
+      await browser.close();
+    }
+    throw new Error(
+      "An error occurred while accessing the site: " + error.message
+    );
+  }
+}
+
+// async function uploadBufferToGCS(buffer, destFileName) {
+//   const bucket = storage.bucket(bucketName);
+//   const file = bucket.file(destFileName);
+//   const stream = file.createWriteStream({
+//     metadata: {
+//       contentType: "image/png",
+//     },
+//   });
+
+//   return new Promise((resolve, reject) => {
+//     stream.on("error", (err) => {
+//       console.error("Stream to GCS had an error", err);
+//       reject(err);
+//     });
+
+//     stream.on("finish", () => {
+//       console.log(
+//         `The file was uploaded successfully to ${bucketName}/${destFileName}`
+//       );
+//       resolve(`https://storage.googleapis.com/${bucketName}/${destFileName}`);
+//     });
+
+//     stream.end(buffer);
+//   });
+// }
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
